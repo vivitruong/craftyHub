d68371eb6333054b52d1f87b90f13ab03bc950d1
vivitruong <vivitruong1506@gmail.com>
2024-03-10 22:45:37 -0500

    add seed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,28 +1,35 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .products import seed_products, undo_products
+from .images import seed_images, undo_images
+from .reviews import seed_reviews, undo_reviews
+from .categories import seed_categories, undo_categories
+from .purchases import seed_purchases, undo_purchases
+from .shops import seed_shops, undo_shops
 
-from app.models.db import db, environment, SCHEMA
-
-# Creates a seed group to hold our commands
-# So we can type `flask seed --help`
+# creates a seed group to hold our commands (`flask seed --help`)
 seed_commands = AppGroup('seed')
 
 
-# Creates the `flask seed all` command
+# creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
-    if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
-        undo_users()
     seed_users()
-    # Add other seed functions here
+    seed_products()
+    seed_images()
+    seed_reviews()
+    seed_categories()
+    seed_purchases()
+    seed_shops()
 
 
-# Creates the `flask seed undo` command
+# creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    # Add other undo functions here
+    undo_products()
+    undo_images()
+    undo_reviews()
+    undo_categories()
+    undo_purchases()
+    undo_shops()
