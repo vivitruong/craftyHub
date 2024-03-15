2d30149e77c984e25525dff79e649a31d55aa275
vivitruong <vivitruong1506@gmail.com>
2024-03-14 20:37:53 -0500

    add image

---
+++ react-app/src/store/images.js
@@ -0,0 +1,38 @@
+const ADD_IMAGE = 'images/ADD_IMAGE'
+const addImages = (newImage) => ({
+    type: ADD_IMAGE,
+    newImage
+  })
+
+  export const uploadImages = (imageData) => async (dispatch) => {
+    const response = await fetch(`/api/images`, {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "multipart/form-data",
+      // },
+      body: imageData
+    })
+
+    if (response.ok) {
+      const newImage = await response.json()
+      dispatch(addImages(newImage));
+      return newImage;
+    }
+  }
+
+  const imageReducer = (state = {}, action) => {
+    let newState = {}
+    switch (action.type) {
+
+      case ADD_IMAGE: {
+        newState = { ...state }
+        newState[action.newImage.id] = action.newImage
+        return newState
+      }
+
+      default:
+        return state;
+    }
+  }
+
+  export default imageReducer
