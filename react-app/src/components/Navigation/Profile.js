d45759b3f4adb94eed865e7006fa51195855c2a3
vivitruong <vivitruong1506@gmail.com>
2024-03-25 20:45:50 -0500

    fixing profile

--- react-app/src/components/Navigation/Profile.js
+++ react-app/src/components/Navigation/Profile.js
@@ -7,3 +7,28 @@ import purchases from '../CSS/Images/purchases.svg'
 import { logout } from '../../store/session';
 import signOut from '../CSS/Images/sign-out.svg'
 import shop from '../CSS/Images/shop.svg'
+
+const Profile = ({ user, setSelectedCategory }) => {
+    const dispatch = useDispatch();
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const onLogout = () => {
+      dispatch(logout());
+    }
+
+    const openMenu = () => {
+      if (showDropdown) return;
+      setShowDropdown(true);
+    };
+
+    useEffect(() => {
+      if (!showDropdown) return;
+
+      const closeMenu = () => {
+        setShowDropdown(false);
+      };
+
+      document.addEventListener('click', closeMenu);
+
+      return () => document.removeEventListener("click", closeMenu);
+    }, [showDropdown]);
