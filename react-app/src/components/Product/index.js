6593afff21c9fac1e4d1dc2266533a8f2d6a62b1
vivitruong <vivitruong1506@gmail.com>
2024-03-26 19:27:28 -0500

    will need to add return

--- react-app/src/components/Product/index.js
+++ react-app/src/components/Product/index.js
@@ -34,3 +34,98 @@ const Product = () => {
 
     const roundedStars = Math.floor(product[productId]?.avg_stars)
     const difference = product[productId]?.avg_stars - roundedStars
+
+    useEffect(() => {
+        dispatch(getUsers())
+        document.documentElement.scrollTop = 0;
+      }, [])
+
+      useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+      }, [cart])
+
+
+      useEffect(() => {
+        const response = dispatch(findProductById(productId))
+        if (response) setSelectedImage(product[productId]?.images[0])
+        displayRating()
+      }, [roundedStars])
+
+
+      const addToCart = (selectedProduct) => {
+
+        if (!user) {
+          setShowSignIn(true)
+          return
+        }
+
+        if (cart && cart.length > 0) {
+          let findItem = cart.filter((item, i) => item.id === selectedProduct.id)
+          let newCart = [...cart]
+
+          if (findItem[0]) {
+            findItem[0].quantity++
+          } else {
+            findItem = {
+              ...selectedProduct,
+              quantity: 1
+            }
+            newCart.push(findItem)
+          }
+          setCart(newCart)
+
+        } else {
+          selectedProduct.quantity = 1
+          setCart([selectedProduct])
+        }
+        setNotification(true)
+        setCount(count + 1)
+      }
+
+      const displayRating = () => {
+        const ratingArr = []
+        for (let i = 0; i < roundedStars; i++) {
+          ratingArr.push(filledStar)
+        }
+
+        if (difference >= 0.5) ratingArr.push(halfStar)
+
+        const remainder = 5 - ratingArr.length
+        if (ratingArr.length > 0 && remainder > 0) {
+          let index = 0
+          while (index < remainder) {
+            ratingArr.push(emptyStar)
+            index += 1
+          }
+        }
+        setRating(ratingArr)
+      }
+
+      const starsDisplay = (starCount) => {
+        return (
+          <>
+            {starCount.map((star, i) => {
+              return (
+                <img className='star-display-rating' src={star} key={i} alt='star'></img>
+              )
+            })}
+          </>
+        )
+      }
+
+      const formatDate = (dateTime) => {
+        let month = dateTime.split(" ")[2]
+        let day = dateTime.split(" ")[1]
+        if (day[0] === '0') day = day.slice(1)
+        let year = dateTime.split(" ")[3]
+
+        return `${month} ${day}, ${year}`
+      }
+
+      console.log(product[productId])
+
+      if (!product[productId]) {
+        return (
+          <NotFound />
+        )
+      }
