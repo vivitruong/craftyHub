0444e8ce7984f56e5df8d6aed5ab3cb8264442e5
vivitruong <vivitruong1506@gmail.com>
2024-03-27 22:10:59 -0500

    finish edit product

--- react-app/src/components/Product/EditProduct.js
+++ react-app/src/components/Product/EditProduct.js
@@ -26,3 +26,126 @@ const EditProduct = ({ productId, setShowEditForm }) => {
         if (description.length > 1000) errors.push('Description: Description exceeds 1000 character limit')
         setErrors(errors)
       }, [name, category, price, description])
+
+      const handleEdit = async (e) => {
+        e.preventDefault()
+
+        if (errors.length > 0) return
+
+        const productData = {
+          product_id: productId,
+          category,
+          name,
+          price,
+          description
+        }
+
+        const response = await dispatch(updateProduct(productData))
+        if (response) setShowEditForm(false)
+      }
+
+      return (
+        <>
+          <form onSubmit={handleEdit} className='edit-form-outer'>
+            <div className='edit-main'>
+              <div className="edit-form-upper">
+                <button className="edit-return-button" onClick={() => { setShowEditForm(false) }}>Return to Shop Manager</button>
+                <div className='edit-header'>Edit your Listing</div>
+              </div>
+              <div className='edit-field'>
+                <div className='edit-field-inner'>
+                  <label className='edit-label'>Title *</label>
+                  <span className='edit-instructions'>Update the keywords used to search for your item.</span>
+                </div>
+                {errors?.map((error, i) => {
+                  if (error.split(":")[0] === 'Name')
+                    return (
+                      <div key={i} className='edit-product-errors'>-{error.split(":")[1]}</div>
+                    )
+                })}
+                <div>
+                  <textarea
+                    type='text'
+                    className='edit-text-area-name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    maxLength={141}
+                  />
+                </div>
+              </div>
+              <div className='edit-field'>
+                <div className='edit-field-inner'>
+                  <label className='edit-label'>Category *</label>
+                  <span className='edit-instructions'>Change the category your product is listed under.</span>
+                </div>
+                {errors?.map((error, i) => {
+                  if (error.split(":")[0] === 'Category')
+                    return (
+                      <div key={i} className='edit-product-errors'>-{error.split(":")[1]}</div>
+                    )
+                })}
+                <div className='select-outer'>
+                  <select
+                    htmlFor='category'
+                    name='category'
+                    className='edit-input'
+                    onChange={(e) => setCategory(e.target.value)}
+                    value={category}
+                  >
+                    {categories?.map((category) => {
+                      return (
+                        <option value={category.name}>{category.display_name}</option>
+                      )
+                    })}
+                  </select>
+                </div>
+              </div>
+              <div className='edit-field'>
+                <div className='edit-field-inner'>
+                  <label className='edit-label'>Price *</label>
+                  <span className='edit-instructions'>Update the price to factor in the cost of labor, materials, and any business expenses.</span>
+                </div>
+                {errors?.map((error, i) => {
+                  if (error.split(":")[0] === 'Price')
+                    return (
+                      <div key={i} className='edit-product-errors'>-{error.split(":")[1]}</div>
+                    )
+                })}
+                <input
+                  type='number'
+                  step={0.01}
+                  className='edit-input'
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  min={0}
+                />
+              </div>
+              <div className='edit-field'>
+                <div className='edit-field-inner'>
+                  <label className='edit-label'>Description *</label>
+                  <span className='edit-instructions'>Modify the description to provide an overview of your item's features.</span>
+                </div>
+                {errors?.map((error, i) => {
+                  if (error.split(":")[0] === 'Description')
+                    return (
+                      <div key={i} className='edit-product-errors'>-{error.split(":")[1]}</div>
+                    )
+                })}
+                <textarea
+                  type='text'
+                  className='edit-text-area'
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  maxLength={1001}
+                />
+              </div>
+              <div className='edit-save-button-outer'>
+                <button type="submit" className='edit-save-button'>Save and Update</button>
+              </div>
+            </div>
+          </form>
+        </>
+      )
+    }
+
+    export default EditProduct
