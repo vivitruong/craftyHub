6774773239e30ccd3a9be209ba99de26ce1396db
vivitruong <vivitruong1506@gmail.com>
2024-04-02 21:45:13 -0500

    ad rating

---
+++ react-app/src/components/Product/Rating.js
@@ -0,0 +1,22 @@
+import star from '../CSS/Images/filled-star.svg'
+import unFilledStar from '../CSS/Images/blank-star.svg'
+import halfStar from '../CSS/Images/half-star.svg'
+import unFilledStarRating from '../CSS/Images/review-star-grey.svg'
+import filledStarRating from '../CSS/Images/review-star-black.svg'
+
+export const halfStars = [halfStar, unFilledStar, unFilledStar, unFilledStar, unFilledStar]
+export const oneStar = [star, unFilledStar, unFilledStar, unFilledStar, unFilledStar]
+export const oneHalfStar = [star, halfStar, unFilledStar, unFilledStar, unFilledStar]
+export const twoStar = [star, star, unFilledStar, unFilledStar, unFilledStar]
+export const twoHalfStar = [star, star, halfStar, unFilledStar, unFilledStar]
+export const threeStar = [star, star, star, unFilledStar, unFilledStar]
+export const threeHalfStar = [star, star, star, halfStar, unFilledStar]
+export const fourStar = [star, star, star, star, unFilledStar]
+export const fourHalfStar = [star, star, star, star, halfStar]
+export const fiveStar = [star, star, star, star, star]
+
+export const ratingOneStar = [filledStarRating, unFilledStarRating, unFilledStarRating, unFilledStarRating, unFilledStarRating]
+export const ratingTwoStar = [filledStarRating, filledStarRating, unFilledStarRating, unFilledStarRating, unFilledStarRating]
+export const ratingThreeStar = [filledStarRating, filledStarRating, filledStarRating, unFilledStarRating, unFilledStarRating]
+export const ratingFourStar = [filledStarRating, filledStarRating, filledStarRating, filledStarRating, unFilledStarRating]
+export const ratingFiveStar = [filledStarRating, filledStarRating, filledStarRating, filledStarRating, filledStarRating]
