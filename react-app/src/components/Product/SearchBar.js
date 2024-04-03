9475e799a7bf91e8abc8fc0b85b8b7dfbdf47945
vivitruong <vivitruong1506@gmail.com>
2024-04-02 21:48:02 -0500

    add search bar

--- react-app/src/components/Product/SearchBar.js
+++ react-app/src/components/Product/SearchBar.js
@@ -12,3 +12,55 @@ const SearchBar = ({setSelectedCategory}) => {
     const user = useSelector(state => state.session.user)
     const [keyword, setKeyword] = useState("")
     const [errors, setErrors] = useState([])
+
+    useEffect(() => {
+        if (keyword && keyword.trim().length >= 1) {
+          setErrors([])
+        }
+      }, [keyword])
+
+      const handleSearch = async (e) => {
+        e.preventDefault()
+
+        // if (keyword.length === 0 || keyword.trim().length === 0) {
+        //   setErrors(['Search keywords must include at least one character'])
+        //   return
+        // }
+
+        const response = await dispatch(findProductsBySearch(keyword))
+        if (response) {
+          history.push(`/search/${keyword}`)
+        }
+
+        setSelectedCategory()
+        setKeyword("")
+        setErrors([])
+      }
+
+
+      return (
+        <div className={user ? 'searchBar-main' : 'searchBar-main-noUser'}>
+          <div>
+            {errors?.map((error, i) => {
+                return (
+                  <div key={i} className='search-errors'>{error}</div>
+                )
+              })}
+          </div>
+          <div className='navBar-searchBar-outer'>
+            <form onSubmit={handleSearch} className="searchBar-form">
+              <input
+                placeholder='Search for anything'
+                className='searchBar-input'
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
+                maxLength="140"
+              />
+              <button type='submit' className='searchBar-button'><img src={magnifyingGlass} className='searchBar-glass' alt='search'></img></button>
+            </form>
+          </div>
+        </div>
+      )
+    }
+
+    export default SearchBar
