87b5f785178abaf81c4bbb30c6caa7eb7bef11c1
vivitruong <vivitruong1506@gmail.com>
2024-04-05 22:50:30 -0500

    add shop

--- react-app/src/components/Shop/index.js
+++ react-app/src/components/Shop/index.js
@@ -4,3 +4,21 @@ import { addNewProduct, getAllProducts } from '../../store/products';
 import { editUser } from '../../store/session';
 import '../CSS/Shop.css'
 import ImageUpload from './ImageUpload';
+const Shop = () => {
+const dispatch = useDispatch()
+  const user = useSelector(state => state.session.user)
+  const categories = useSelector(state => Object.values(state.categories))
+  const [productId, setProductId] = useState()
+  const [page, setPage] = useState(0)
+  const [shopName, setShopName] = useState(user.shop_name ? user.shop_name : "")
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState(0)
+  const [description, setDescription] = useState("")
+  const [category, setCategory] = useState("")
+  const [shopErrors, setShopErrors] = useState([])
+  const [errors, setErrors] = useState([])
+  const [checkDuplicate, setCheckDuplicate] = useState(false)
+
+
+
+}
