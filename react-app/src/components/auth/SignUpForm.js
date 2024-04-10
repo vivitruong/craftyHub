5328224ce9dfbe34b967f055c68ec42bfccfc2d2
vivitruong <vivitruong1506@gmail.com>
2024-04-09 21:51:15 -0500

    add signup

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,3 +2,11 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { signUp } from '../../store/session';
 import '../CSS/SignUpForm.css'
+
+const SignUpForm = ({ setShowRegister, setShowSignIn }) => {
+    const [errors, setErrors] = useState([]);
+    const [first_name, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const dispatch = useDispatch();
