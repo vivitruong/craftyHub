8c60b1e300f3f19ef01d3d4860a8ab20cfb3a706
vivitruong <vivitruong1506@gmail.com>
2024-05-07 22:14:52 -0500

    add index

--- react-app/src/components/Shop/index.js
+++ react-app/src/components/Shop/index.js
@@ -95,8 +95,35 @@ const dispatch = useDispatch()
     }
   }
   return (
-    <>
-    </>
+    <div className='sell-product-main'>
+      {page === 0 && <>
+      <div className='main-shop-outer'>
+        <div className='sell-product-main-header'>Millions of shopper can't wait to see what you have in store</div>
+        <button className='get-started-button' onClick={checkShopName}>Get started</button>
+      </div>
+      <div className='sell-product-bottom'>
+        <div className='product-bottom-icons'>
+          <div className='product-bottom-inner'>
+            <div><img src="https://img0.etsystatic.com/site-assets/sell-page/Sell_Page_Icon_Great_Value.svg" className='bottom-icon-images' alt=""></img></div>
+            <div className='product-bottom-header'>Great value</div>
+          </div>
+        </div>
+        <div  className='product-bottom-icons'>
+        <div className='product-bottom-inner'>
+              <div><img src="https://img0.etsystatic.com/site-assets/sell-page/Sell_Page_Icon_Tools.svg" className='bottom-icon-images' alt=""></img></div>
+              <div className='product-bottom-header'>Powerful tools</div>
+            </div>
+        </div>
+        <div className='product-bottom-icon-last'>
+            <div className='product-bottom-inner'>
+              <div><img src="https://img0.etsystatic.com/site-assets/sell-page/Sell_Page_Icon_Education.svg" className='bottom-icon-images' alt=""></img></div>
+              <div className='product-bottom-header'>Support and education</div>
+            </div>
+          </div>
+      </div>
+      </>}
+
+    </div>
 
   )
 
