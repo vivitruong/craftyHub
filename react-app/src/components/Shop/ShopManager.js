260431adac683afaa9f78988ff7ca3a52632d9e7
vivitruong <vivitruong1506@gmail.com>
2024-05-09 22:41:19 -0500

    inital commit for shopmanager

---
+++ react-app/src/components/Shop/ShopManager.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import {Link, useHistory} from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { loadProductsByOwner, findProductById, removeProduct } from "../../store/products";
+import '../CSS/ShopManager.css';
+import {Modal} from '../Context/modal';
+import EditProduct from "../Product/EditProduct";
+import rightAngle from '../CSS/Images/right_angle.svg'
+
+const ShopManager = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const user = useSelector(state => state.session.user)
+    const products = useSelector(state => Object.values(state.products))
+    const [productId, setProductId] = useState()
+    const [deleteConfirmation, setDeleteConfirmation] = useState(false)
+    const [showEditForm, setShowEditForm] = useState(false)
+
+    useEffect(() => {
+        dispatch(loadProductsByOwner(user.id))
+        if(products.length === 0) {
+            history.push('/shop')
+        }
+    },[products.length])
+
+    return(<>
+    </>)
+
+}
