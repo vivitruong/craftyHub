e7edd91b6992f834017db1794863c72ce2c5d33c
vivitruong <vivitruong1506@gmail.com>
2024-05-15 22:27:02 -0500

    add setmUltil image{

--- react-app/src/components/Shop/ImageUpload.js
+++ react-app/src/components/Shop/ImageUpload.js
@@ -6,3 +6,31 @@ import '../CSS/ImageUpload.css'
 import photo from '../CSS/Images/photo.svg'
 import whiteX from '../CSS/Images/white-x.svg'
 import { uploadImages } from "../../store/images";
+
+const ImageUpload = ({productId}) => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [multiImages, setMultiImage] = useState([])
+    const [image, setImage] = useState(null);
+    const [image2, setImage2] = useState(null);
+    const [image3, setImage3] = useState(null);
+    const [image4, setImage4] = useState(null);
+    const [imageLoading, setImageLoading] = useState(null);
+    const user = useSelector(state => state.session.user)
+    const [errors, setErrors] = useState([])
+    const [brokenImage, setBrokenImage] = useState(state)
+    const [brokenImage2, setBrokenImage2] = useState(state)
+    const [brokenImage3, setBrokenImage3] = useState(state)
+    const [brokenImage4, setBrokenImage4] = useState(state)
+
+    useEffect(() => {
+        if(image4) setMultiImage([image, image2, image3, image4])
+        else if (image3) setMultiImage([image, image2, image3])
+        else if(image2) setMultiImage([image, image2])
+        else if(image) setMultiImage([image])
+        else setMultiImage([])
+
+        if(image || image2 || image3 || image4) setErrors([])
+
+    })
+}
